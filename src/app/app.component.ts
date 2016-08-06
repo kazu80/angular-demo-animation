c0250fa3d9852cad537e7546bf720ad112e86dee
monkick <saruyama.monki@gmail.com>
2016-08-06 16:40:20 +0900

    create initial html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, trigger, state, style, transition, animate, OnInit} from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  animations: [
+    trigger('buttonState', [
+      state('play', style({
+        "background-image": 'url("/images/play.svg")',
+      })),
+      state('stop', style({
+        "background-image": 'url("/images/stop.svg")',
+      })),
+      transition('play => stop', animate('100ms ease-in')),
+      transition('stop => play', animate('100ms ease-out'))
+    ])
+  ]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  public state : string;
+
+    ngOnInit(): void {
+      this.state = "stop";
+    }
+
+    private onClick() {
+      this.state = this.state === "play" ? "stop" : "play";
+    }
 }
